cabb551bc64364fbf5f9c9b9ceb096775890560b
eduardobcavalcante <eduardobcavalcante@hotmail.com>
2024-09-10 14:56:08 -0300

    enviando a solução da questao10 para a listaexercicio02

---
+++ listaexercicio02/exercicio10.js
@@ -0,0 +1,27 @@
+/* Objetivo: Crie um array de objetos vendas, onde cada objeto tem produto,
+quantidade e valor. Use forEach para calcular o valor total de vendas de
+todos os produtos. */
+
+venda = [
+    {produto: "a",
+     quantidade: 3,
+     valor: 8,
+},
+    {produto: "b",
+     quantidade: 10,
+     valor: 7,
+},
+    {produto: "c",
+     quantidade: 1,
+     valor: 45,
+},
+
+]
+
+somaValor = 0
+venda.forEach((item)=>{
+    if(venda.produto!=0){
+        somaValor+= item.quantidade * item.valor 
+    }
+    })
+    console.log(`A valor total das vendas é ${somaValor}`)
